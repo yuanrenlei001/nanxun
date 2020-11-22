fd9cecf7c52aa667a5d899a35de73ca880ad25b5
叫我〜99 <oncwnuBDZ4g8Djeb5W9sovYq93kI@git.weixin.qq.com>
2020-11-22 21:58:52 +0800

    静态页面的完成

--- pages/index/index.js
+++ pages/index/index.js
@@ -22,6 +22,38 @@ Page({
       url: '../notice/notice',
     })
   },
+  goUrl:function(){
+    wx.navigateBackMiniProgram({
+
+      appId:'wx0e6ed4f51db9d078',
+      
+      path: 'pages/market/web/index?AllianceID=1294595&sid=3672763&ouid=link&from=https%3A%2F%2Fm.ctrip.com%2Fwebapp%2Fhotel%2Fhoteldetail%2F8300215.html%3Fdaylater%3D2%26AllianceID%3D1294595%26sid%3D3672763%26ouid%3Dlink',
+      
+      envVersion: 'trial',
+      
+      extraData: {
+      
+      openId: '123'    //需要传递给目标小程序的数据，目标小程序可在 App.onLaunch，App.onShow 中获取到这份数据
+      
+      },
+      
+      success(res) {
+      
+      // 打开其他小程序成功同步触发
+      
+      wx.showToast({
+      
+      title: '跳转成功'
+      
+      })
+      
+      
+      
+      
+      }
+      
+      })
+  },
   // 跳转资讯列表
   noticeList:function(){
     wx.navigateTo({
