416193d226fef7bb7843657c3e3bc87ee6d11606
shuangfei.yao <shuangfei.yao@eslink.com>
2020-12-09 09:33:56 +0800

    up

--- pages/map/index.js
+++ pages/map/index.js
@@ -1,20 +1,81 @@
 // pages/map/index.js
+var amapFile = require('../../libs/amap-wx.js');
+var markersData = [];
 const app = getApp();
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-
+    markers: [],
+    latitude: '',
+    longitude: '',
+    textData: {}
   },
-
+  /**
+   * 
+   * 高德地图
+   */
+  makertap: function(e) {
+    var id = e.markerId;
+    var that = this;
+    that.showMarkerInfo(markersData,id);
+    that.changeMarkerColor(markersData,id);
+  },
+  onLoad: function() {
+    var that = this;
+    var myAmapFun = new amapFile.AMapWX({key:'458c50224024594bb3053a8e141349b6'});
+    myAmapFun.getPoiAround({
+      iconPathSelected: '/images/map/Unchecked/scenic_spot.png', //如：..­/..­/img/marker_checked.png
+      iconPath: '/images/map/Unchecked/restaurant.png', //如：..­/..­/img/marker.png
+      success: function(data){
+        markersData = data.markers;
+        that.setData({
+          markers: markersData
+        });
+        that.setData({
+          latitude: markersData[0].latitude
+        });
+        that.setData({
+          longitude: markersData[0].longitude
+        });
+        that.showMarkerInfo(markersData,0);
+      },
+      fail: function(info){
+        wx.showModal({title:info.errMsg})
+      }
+    })
+  },
+  showMarkerInfo: function(data,i){
+    var that = this;
+    that.setData({
+      textData: {
+        name: data[i].name,
+        desc: data[i].address
+      }
+    });
+  },
+  changeMarkerColor: function(data,i){
+    var that = this;
+    var markers = [];
+    for(var j = 0; j < data.length; j++){
+      if(j==i){
+        data[j].iconPath = "/images/map/Unchecked/hotel.png"; //如：..­/..­/img/marker_checked.png
+      }else{
+        data[j].iconPath = "/images/map/Unchecked/server.png"; //如：..­/..­/img/marker.png
+      }
+      markers.push(data[j]);
+    }
+    that.setData({
+      markers: markers
+    });
+  },
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  // onLoad: function (options) {
 
-  },
+  // },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
