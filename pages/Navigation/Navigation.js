fbd6ea5a78b2540320a3cbd7ae7f764033246d90
shuangfei.yao <shuangfei.yao@eslink.com>
2021-01-05 20:46:40 +0800

    daohang

---
+++ pages/Navigation/Navigation.js
@@ -0,0 +1,90 @@
+// pages/Navigation/Navigation.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    console.log(options);
+    let data = {
+      latitude: options.latitude,
+      longitude: options.longitude,
+      address: options.address
+    }
+    this.intoMap(data)
+  },
+  // 导航
+  intoMap:function(data){
+    wx.getLocation({
+      type: 'gcj02', //返回可以用于wx.openLocation的经纬度
+      success: function (res) {  //因为这里得到的是你当前位置的经纬度
+        wx.openLocation({        //所以这里会显示你当前的位置
+          latitude: parseFloat(data.latitude),
+          longitude: parseFloat(data.longitude),
+          name:data.address,
+          scale: 18
+        });
+      }
+    })
+  },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    setTimeout(()=>{
+      wx.switchTab({
+        url: '/pages/foryou/index'
+      })
+    },3000)
+   
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
